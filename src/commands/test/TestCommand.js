ed8b9598e2606f81cd6b1260a1d5a4d8b62a5d72
AaronHotcher <62381091+AaronHotcher@users.noreply.github.com>
2020-12-22 17:02:53 +0000

    Add files via upload

---
+++ src/commands/test/TestCommand.js
@@ -0,0 +1,42 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+
+  module.exports = class TestCommand extends BaseCommand {
+    constructor() {
+      super('NewTicket', 'New', 'TicketCreate', []);
+    }
+  
+    
+      
+    },
+     function (bot, r, msg, args) {
+  r.table('tickets').get(msg.author.id).run((err, callback) => {
+    if (err)
+      return bot.error(err.stack);
+    if (callback && (callback.length !== 0)) {
+      if (!callback.closed) {
+        msg.channel.createMessage(':x: | You already have an open ticket (<#' + callback.channel + '>)');
+        return;
+      }
+    }
+    r.table('tickets').orderBy({ index: r.desc('case') }).run((err, callback) => {
+      if (err)
+        return bot.error(err.stack);
+      msg.channel.guild.createChannel('ticket-' + (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4), 0, 'New ticket', bot.config.category).then((channel) => {
+        r.table('tickets').insert({
+          id: msg.author.id,
+          case: (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4),
+          channel: channel.id
+        }).run();
+        channel.createMessage({
+          embed: {
+            description: 'ID: ' + (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4) + '\nMessage: ' + (args.length > 0 ? args.join(' ') : 'None')
+          }
+        });
+      }).catch((err) => {
+        bot.error(err.stack);
+        msg.channel.createMessage(':x: | An error has occurred!');
+      });
+    });
+  });
+}
+  
